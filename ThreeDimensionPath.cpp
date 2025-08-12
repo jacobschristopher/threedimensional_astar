6cb43b4433e6eb00e9e8dc767efa2f78546becd1
Chris Jacobs <chejac2000@gmail.com>
2025-08-12 19:23:03 -0400

    Reorganizing files

--- ThreeDimensionPath.cpp
+++ ThreeDimensionPath.cpp
@@ -6,7 +6,32 @@
 #include <set>
 #include <math.h>
 
-int main() 
+int main(int argc, char* argv[]) 
 {
-    std::cout << "Hello World" << std::endl;
+    // Start up GLUT
+    glutInit(&argc, argv);
+
+    // Create the graphics window, giving width, height and title text
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition(80,80);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Three Dimensional Pathfinding using A*");
+
+    // Specify clear background color
+    glEnable(GL_DEPTH_TEST);
+
+    // Set up callback routines
+    glutDisplayFunc(display);
+    glutKeyboardFunc(handleKey);
+    glutSpecialFunc(specialInput);
+
+    glClearColor(0,0,0,0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    std::cout << "Starting program..." << '\n';
+    std::cout << "Welcome." << '\n';
+
+    glutMainLoop();
+    return 0;
 }
